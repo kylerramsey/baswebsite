f6489bc24a84f5dbcfce25a1ca22118113622601
Kyler Ramsey <104465783+kylerramsey@users.noreply.github.com>
2022-09-16 15:47:20 -0400

    changes to navbar again, and site layout

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector('.navbar');
+window.onscroll = () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
