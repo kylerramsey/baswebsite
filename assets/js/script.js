1f215eac9e91ae61f72764f41a0645976857e1a5
Kyler Ramsey <104465783+kylerramsey@users.noreply.github.com>
2022-09-23 20:08:25 -0400

    added development process section, scrollbar for language logos, cleaned up many sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,30 @@ window.onscroll = () => {
         navbar.classList.remove('nav-active');
     }
 };
+
+// LOGO SLIDER FUNCTION
+
+var $jq = jQuery.noConflict();
+
+$jq(document).ready(function(){
+    $jq('.logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
